30ffbcde0a3d976d6f02a22c6cc519e0a970f61e
AhmedJarraya999 <96777472+AhmedJarraya999@users.noreply.github.com>
2023-05-04 00:57:10 +0100

    fixing status mosule issue not available latest version in django

--- users/urls.py
+++ users/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('users/', views.user_list),
+    #path('users/', views.user_list),
     path('users/<int:pk>/', views.user_detail),
     path('users/create/', views.user_create),
     path('users/update/<int:pk>/', views.user_update),
     path('users/delete/<int:pk>/', views.user_delete),
-    path('users/get', views.user_list),
-    path('',views.user_list)
+    path('users/get', views.user_list)
+  #  path('',views.user_list)
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import HttpResponse, JsonResponse, Http404, HttpResponseBadRequest, HttpResponseServerError
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
 from rest_framework import generics
@@ -27,9 +27,8 @@ def user_create(request):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
 @csrf_exempt
 @api_view(['GET'])
 def user_detail(request, pk):
