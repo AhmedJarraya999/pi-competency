381481fef1ad7c38d04f7e645f61a8296a354227
AhmedJarraya999 <96777472+AhmedJarraya999@users.noreply.github.com>
2023-05-04 04:30:11 +0100

    upload studyplan

--- pi/urls.py
+++ pi/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from users import urls
+#from users import urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
